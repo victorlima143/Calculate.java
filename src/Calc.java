660affc41ff1deaa7633f106479896a957e15bd8
victorlima143 <victorlima143>
2024-04-01 15:11:35 +0100

    Calculadora em Java

---
+++ src/Calc.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Num n1 = new Num();
+        Num n2 = new Num();
+        Num res = new Num();
+
+        System.out.printf("Digite o valor 1:");
+        n1.setValor(scanner.nextInt());
+        System.out.printf("Digite o valor 2:");
+        n2.setValor(scanner.nextInt());
+        res.setValor(n1.getValor() + n2.getValor());
+        System.out.printf("A soma de %d e %d é igual a %d", n1.getValor(), n2.getValor(), res.getValor());
+
+    }
+
+}
---
+++ src/Num.java
@@ -0,0 +1,12 @@
+public class Num {
+    private int valor;
+    public Num(){
+        this.valor = 0;
+    }
+    public void setValor(int valor){
+        this.valor = valor;
+    }
+    public int getValor(){
+        return this.valor;
+    }
+}
